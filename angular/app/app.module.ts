b120ce379b68b4fba12d20f680968d1b0139af2e
Gareth Brown <gareth.brown@mac4013.bjss.co.uk>
2019-05-15 16:38:44 +0100

    Implement basic button component

--- angular/app/app.module.ts
+++ angular/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { TableComponent } from './table/table.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    TableComponent
+    TableComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
