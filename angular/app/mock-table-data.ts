2c49bccea2cf8275529f67e0896020ed5f28c0bc
Gareth Brown <gareth.brown@mac4013.bjss.co.uk>
2019-05-15 15:54:54 +0100

    Modify Table values type to accept any object

--- angular/app/mock-table-data.ts
+++ angular/app/mock-table-data.ts
@@ -1,14 +1,8 @@
 export class Table {
     headings: string[];
-    records: Record[];
+    records: object[];
 }
 
-interface Record {
-    folio: string
-    title: string
-    from: string
-    to: string
-}
 
 export const TABLE: Table = {
     headings: [
@@ -22,37 +16,37 @@ export const TABLE: Table = {
             folio: '5600',
             title: 'The Solent and Approaches',
             from: '26/05/16',
-            to: '26/05/16'
+            to: '26/05/16',
         },
         {
             folio: '5600',
             title: 'East Devon and Dorset Coast, Exmouth to Christchurch',
             from: '26/05/16',
-            to: '26/05/16'
+            to: '26/05/16',
         },
         {
             folio: '5600',
             title: 'The West Country, Falmouth to Teignmouth',
             from: '26/05/16',
-            to: '26/05/16'
+            to: '26/05/16',
         },
         {
             folio: '5600',
             title: 'Falmouth to Hartland Point including the Isles of Scilly',
             from: '26/05/16',
-            to: '26/05/16'
+            to: '26/05/16',
         },
         {
             folio: '5600',
             title: 'The Channel Islands',
             from: '26/05/16',
-            to: '26/05/16'
+            to: '26/05/16',
         },
         {
             folio: '5600',
             title: 'Chichester to Ramsgate and Calais to Oostende',
             from: '26/05/16',
-            to: '26/05/16'
+            to: '26/05/16',
         },
     ]
 }
