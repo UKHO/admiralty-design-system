20b79067648fe05738f6d64e2e6bf4713ab1f61b
Gareth Brown <gareth.brown@mac4013.bjss.co.uk>
2019-05-15 14:06:24 +0100

    Import CardComponent into app.component.spec

--- angular/app/app.component.spec.ts
+++ angular/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
+import { CardComponent } from './card/card.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -11,7 +12,8 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
-        TableComponent
+        TableComponent,
+        CardComponent
       ],
     }).compileComponents();
   }));
