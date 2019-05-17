09a032d376e17feb429a0d36730a35ed0c1e3cbc
Gareth Brown <gareth.brown@mac4013.bjss.co.uk>
2019-05-17 12:11:37 +0100

    Implement navigation javascript nav-enhance

--- angular/app/navigation/navigation.component.ts
+++ angular/app/navigation/navigation.component.ts
@@ -1,15 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
 import { Navigation } from './mocknavigation';
+import { enhanceMenu }from './nav-enhance';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements AfterViewInit {
 
   constructor() { }
 
   @Input() navigation: Navigation;
 
+  ngAfterViewInit() {
+    enhanceMenu();
+  }
 }
