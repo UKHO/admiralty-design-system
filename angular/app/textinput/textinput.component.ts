ed1e0cc37e8f88c51b1571dbd87a46626b17377a
Gareth Brown <gareth.brown@mac4013.bjss.co.uk>
2019-05-20 11:25:50 +0100

    Add disabled functionality to text input

--- angular/app/textinput/textinput.component.ts
+++ angular/app/textinput/textinput.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-textinput',
   templateUrl: './textinput.component.html',
   styleUrls: ['./textinput.component.scss']
 })
-export class TextinputComponent implements OnInit {
+export class TextinputComponent {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  @Input() isDisabled: boolean;
 
 }
