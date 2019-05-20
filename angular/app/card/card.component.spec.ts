929691f7c44ec885dbb237ebfe4647a98aaebc4a
Jerome Wilson <jerome.wilson@bjss.com>
2019-05-20 12:22:55 +0100

    added tests to card component

--- angular/app/card/card.component.spec.ts
+++ angular/app/card/card.component.spec.ts
@@ -1,25 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
+import { DebugElement } from '@angular/core';
 
 describe('CardComponent', () => {
   let component: CardComponent;
   let fixture: ComponentFixture<CardComponent>;
+  let compiled: DebugElement['nativeElement'];
+
+  const testTitle = 'test title';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CardComponent ]
     })
-    .compileComponents();
+        .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the title in an H6', () => {
+    component.title = testTitle;
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
+    const text = compiled.querySelector('h6');
+    expect(text.textContent.trim()).toEqual(testTitle)
+  });
+
+  it('should not render an H6 if no title is passed', () => {
+    component.title = '';
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
+    const title = compiled.querySelector('H6');
+    expect(title).toBeNull()
+  });
+
 });
