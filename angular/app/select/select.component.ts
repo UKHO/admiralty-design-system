89a58aa3d216c6ab669269a745a235e4a401408b
Gareth Brown <gareth.brown@mac4013.bjss.co.uk>
2019-05-21 15:31:50 +0100

    Style expansion pane for select component

--- angular/app/select/select.component.ts
+++ angular/app/select/select.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss']
 })
-export class SelectComponent implements OnInit {
+export class SelectComponent {
 
   constructor() { }
 
-  ngOnInit() {
+  @ViewChild('selectComponent') selectComponent;
+
+  foods: OptionValue[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+  handleChange() {
+    console.log('select has changed', this.selectComponent.panelOpen)
   }
 
+  getIconClass() {
+    return "fa-chevron-down'"
+  }
+
+}
+
+export interface OptionValue {
+  value: string;
+  viewValue: string;
 }
