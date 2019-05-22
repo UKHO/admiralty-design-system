6133eda4058fc7a17f6bc484e91fcc44ce4c0cb9
Gareth Brown <gareth.brown@mac4013.bjss.co.uk>
2019-05-22 13:13:54 +0100

    Add test for radio buttons

--- angular/app/radio/radio.component.spec.ts
+++ angular/app/radio/radio.component.spec.ts
@@ -2,10 +2,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatRadioModule } from '@angular/material';
 
 import { RadioComponent } from './radio.component';
+import { DebugElement } from '@angular/core';
 
 describe('RadioComponent', () => {
   let component: RadioComponent;
   let fixture: ComponentFixture<RadioComponent>;
+  let compiled: DebugElement['nativeElement'];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,5 +25,13 @@ describe('RadioComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('adds is-column class if isColumn is true', () => {
+    component.isColumn = true;
+    fixture.detectChanges()
+    compiled = fixture.nativeElement;
+    const radioGroup = compiled.querySelector('.mat-radio-group')
+    expect(radioGroup.classList.contains('is-column')).toBe(true);
   });
 });
