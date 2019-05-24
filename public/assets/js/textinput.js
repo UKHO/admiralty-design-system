6c6e5116653bb4524c9cc5d62b661056b76954ce
Gareth Brown <gareth.brown@mac4013.bjss.co.uk>
2019-05-24 15:10:51 +0100

    Add textinput validated colour

--- public/assets/js/textinput.js
+++ public/assets/js/textinput.js
@@ -3,12 +3,25 @@ var labelEl = document.getElementById('input-label');
 textinputEl.addEventListener('input', toggleValueClass);
 
 function toggleValueClass(event) {
-    console.log('change event', event.target.value);
     if (event.target.value !== "") {
         if (!labelEl.classList.contains('has-value')) {
             labelEl.classList.add('has-value');
         }
     } else {
         labelEl.classList.remove('has-value');
+    }
+}
+
+var textinputEl3 = document.getElementById('name-input3');
+var labelEl3 = document.getElementById('input-label3');
+textinputEl3.addEventListener('input', toggleValueClass3);
+
+function toggleValueClass3(event) {
+    if (event.target.value !== "") {
+        if (!labelEl3.classList.contains('has-value')) {
+            labelEl3.classList.add('has-value');
+        }
+    } else {
+        labelEl3.classList.remove('has-value');
     }
 }
