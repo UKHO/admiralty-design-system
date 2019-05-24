9b478caa4655d0ed57933d57bfd5e991bb2de0d7
Gareth Brown <gareth.brown@mac4013.bjss.co.uk>
2019-05-24 10:53:09 +0100

    Add angular examples to expansion page

--- angular/app/expansion/expansion.component.ts
+++ angular/app/expansion/expansion.component.ts
@@ -7,13 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpansionComponent {
 
-  isExpanded: boolean = false;
-
-  toggleExpanded() {
-    console.log('toggled')
-    this.isExpanded = !this.isExpanded;
-  }
-  
   constructor() { }
 
+  isExpanded: boolean = false;
+
+  expandText: string = "Read more...";
+  
+  toggleExpanded() {
+    this.isExpanded = !this.isExpanded;
+    this.expandText = this.isExpanded ? "Read less..." : "Read more...";
+  }
+  
 }
