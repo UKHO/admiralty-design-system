c5a6841e822f8f15c9103467fc55ffb68980074f
Antony Derham <antony.derham@ukho.gov.uk>
2019-11-07 14:58:31 +0000

    fix: Export for Phase Banner

--- projects/components/src/lib/phase-banner/phase-banner.module.ts
+++ projects/components/src/lib/phase-banner/phase-banner.module.ts
@@ -6,6 +6,7 @@ export { PhaseBannerComponent } from './phase-banner.component';
 
 @NgModule({
   declarations: [PhaseBannerComponent],
+  exports: [PhaseBannerComponent],
   imports: [CommonModule],
 })
 export class PhaseBannerModule {}
