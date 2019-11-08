3307253956f6a42aaf8ff1c78776b20d09b6143c
Antony Derham <antony.derham@ukho.gov.uk>
2019-11-08 14:08:07 +0000

    fix(components): Export paginator

--- projects/components/src/lib/paginator/paginator.module.ts
+++ projects/components/src/lib/paginator/paginator.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginatorComponent } from './paginator.component';
 
+export { PaginatorComponent } from './paginator.component';
+
 @NgModule({
   declarations: [PaginatorComponent],
+  exports: [PaginatorComponent],
   imports: [CommonModule],
 })
 export class PaginatorModule {}
