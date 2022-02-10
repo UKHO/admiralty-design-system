e163d653e2aad01236e8d2f16411cb7559f1f697
Natalie <natalie.johnson@bjss.com>
2022-02-10 15:16:37 +0000

    feat: make expansion component accessible (#490)
    * feat: make expansion component accessible
    
    * add focus state
    
    * update focus style to be border
    
    * update overflow style
    
    * change margin to padding
    
    * update IDs and add hide/show description
    
    * move style, update stories
    
    * fix story initial params
    
    * change header padding back to margin so focus style is correct and add padding to prevent margin collapsing
    
    * ensure global padding does not affect button

--- components/expansion/expansion.component.ts
+++ components/expansion/expansion.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 let nextId = 0;
 
@@ -13,5 +13,14 @@ export class ExpansionComponent {
   @Input() alignHeadingRight: boolean = false;
   @Input() hideBorder: boolean = false;
 
-  id = `ukho-expansion-${++nextId}`;
+  @Output() change = new EventEmitter<boolean>();
+
+  id: string = `ukho-expansion-${++nextId}`;
+  headerId: string = `${this.id}-header`;
+  contentId: string = `${this.id}-content`;
+
+  onToggle() {
+    this.initialExpanded = !this.initialExpanded;
+    this.change.emit(this.initialExpanded);
+  }
 }
