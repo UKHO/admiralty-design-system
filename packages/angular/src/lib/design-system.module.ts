f7d16f049e8bf4cb94ccbfba674722634a3987c2
natalie johnson <natalie.johnson@bjss.com>
2022-02-11 11:21:16 +0000

    working example

--- packages/angular/src/lib/design-system.module.ts
+++ packages/angular/src/lib/design-system.module.ts
@@ -4,9 +4,6 @@ import { defineCustomElements } from "@ukho/design-system-core/loader";
 import { MyButton, MyComponent } from './stencil-generated/components'
 import {CommonModule} from "@angular/common";
 
-console.log('register web components in design-system.module - this is not called');
-defineCustomElements(window);
-
 const DECLARATIONS = [
   MyButton,
   MyComponent,
@@ -19,9 +16,4 @@ const DECLARATIONS = [
   ],
   exports: DECLARATIONS
 })
-export class DesignSystemModule {
-  constructor() {
-    console.log('register web components in design-system.module - this is not called either');
-    defineCustomElements(window);
-  }
-}
+export class DesignSystemModule {}
