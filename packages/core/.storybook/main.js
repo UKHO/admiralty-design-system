bf21fd1c759b6597149aafb3ea8469c7798fce26
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2023-05-24 10:15:44 +0100

    Upgrade Card stories to Storybook v7

--- packages/core/.storybook/main.js
+++ packages/core/.storybook/main.js
@@ -2,7 +2,8 @@ module.exports = {
   //stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   stories: ['../src/components/button/button.stories.ts',
   '../src/components/breadcrumb/breadcrumb.stories.ts',
-  '../src/components/breadcrumbs/breadcrumbs.stories.ts'],
+  '../src/components/breadcrumbs/breadcrumbs.stories.ts',
+  '../src/components/card/card.stories.ts'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/web-components-vite',
--- packages/core/src/components/card/card.stories.ts
+++ packages/core/src/components/card/card.stories.ts
@@ -1,27 +1,32 @@
-// import { Story } from '@storybook/html';
-// import readme from './readme.md';
-//
-// export default {
-//   title: 'Card',
-//   parameters: {
-//     markdown: readme,
-//   },
-// };
-//
-// export const WithTitle: Story = args => {
-//   return `<admiralty-card heading="${args.heading}">Some content</admiralty-card>`;
-// };
-// WithTitle.args = {
-//   heading: 'I am a card',
-// };
-//
-// export const WithoutTitle: Story = args => {
-//   return `<admiralty-card>Some content</admiralty-card>`;
-// };
-//
-// export const WithHtmlContent: Story = args => {
-//   return `<admiralty-card heading="${args.heading}">You can put any html content in the body of a card. Such as <a href='#'>links</a> or <b>bold text</b></admiralty-card>`;
-// };
-// WithHtmlContent.args = {
-//   heading: 'I am a card',
-// };
+import { Meta, StoryObj } from '@storybook/web-components';
+import { CardComponent } from './card';
+import { html } from 'lit';
+
+const meta: Meta = {
+  component: 'admiralty-card',
+  parameters: {
+    actions: {
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<CardComponent>;
+
+export const WithTitle: Story = { 
+    render: args => html`
+        <admiralty-card heading="${args.heading}">Some content</admiralty-card>`,
+    args: { heading: 'I am a card' }
+};
+
+export const WithoutTitle: Story = {
+    render: args => html`
+        <admiralty-card>Some content</admiralty-card>`
+};
+
+export const WithHtmlContent: Story = {
+    render: args => html`
+        <admiralty-card heading="${args.heading}">You can put any html content in the body of a card. Such as <a href='#'>links</a> or <b>bold text</b></admiralty-card>`,
+    args: { heading: 'I am a card' }
+};
