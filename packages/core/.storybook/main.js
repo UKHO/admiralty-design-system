feeeb2135fecf908f2120a74331d0a56768fd8bd
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2023-05-24 17:03:17 +0000

    Upgrade Filter stories to Storybook v7

--- packages/core/.storybook/main.js
+++ packages/core/.storybook/main.js
@@ -8,7 +8,8 @@ module.exports = {
   '../src/components/colour-block/colour-block.stories.ts',
   '../src/components/dialogue/dialogue.stories.ts',
   '../src/components/expansion/expansion.stories.ts',
-  '../src/components/file-input/file-input.stories.ts'],
+  '../src/components/file-input/file-input.stories.ts',
+  '../src/components/filter/filter.stories.ts'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/web-components-vite',
