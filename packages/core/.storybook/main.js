187e4a2326fa7e437cb6bcd289961579c7f9d427
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2023-06-08 10:58:36 +0000

    Upgrade Radio stories to Storybook v7

--- packages/core/.storybook/main.js
+++ packages/core/.storybook/main.js
@@ -21,7 +21,8 @@ module.exports = {
   '../src/components/link/link.stories.ts',
   '../src/components/paginator/paginator.stories.ts',
   '../src/components/phase-banner/phase-banner.stories.ts',
-  '../src/components/progress-bar/progress-bar.stories.ts'],
+  '../src/components/progress-bar/progress-bar.stories.ts',
+  '../src/components/radio/radio.stories.ts'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/web-components-vite',
