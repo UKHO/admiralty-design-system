8dc9d41294eab71622a910acce2667675feddb36
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2023-06-08 11:10:08 +0000

    Upgrade Radio Group stories to Storybook v7

--- packages/core/src/components/radio-group/radio-group.stories.ts
+++ packages/core/src/components/radio-group/radio-group.stories.ts
@@ -1,38 +1,34 @@
-// import readme from './readme.md';
-// import { Story } from '@storybook/html';
-//
-// export default {
-//   title: 'Forms/Radio Group',
-//   parameters: {
-//     markdown: readme,
-//     actions: {
-//       handles: ['radioChange'],
-//     },
-//   },
-// };
-//
-// export const Basic: Story = args => {
-//   return `
-//     <admiralty-radio-group display-vertical=${args.stack}>
-//       <admiralty-radio name="grp" value="option1">Option 1</admiralty-radio>
-//       <admiralty-radio name="grp" value="option2">Option 2</admiralty-radio>
-//     </admiralty-radio-group>`;
-// };
-// Basic.args = {
-//   stack: true,
-// };
-//
-// export const yesNo: Story = args => {
-//   return `
-//       <h3 id="group-label-boolean">
-//         Do you agree?
-//       </h3>
-//       <admiralty-radio-group display-vertical="true" value=${args.value}>
-//
-//       <admiralty-radio name="grp" value="true">Yes</admiralty-radio>
-//       <admiralty-radio name="grp" value="false">No</admiralty-radio>
-//   </admiralty-radio-group>`;
-// };
-// yesNo.args = {
-//   value: false,
-// };
+import { Meta, StoryObj } from '@storybook/web-components';
+import { RadioGroupComponent } from './radio-group';
+import { html } from 'lit';
+
+const meta: Meta = {
+  component: 'admiralty-radio-group',
+  title: 'Forms/Radio Group',
+  parameters: {
+    actions: {
+      handles: ['radioChange']
+    }
+  }
+};
+
+export default meta;
+
+type Story = StoryObj<RadioGroupComponent>;
+
+export const Basic: Story = { render: args => html`
+  <admiralty-radio-group display-vertical=${args.stack}>
+    <admiralty-radio name="grp" value="option1">Option 1</admiralty-radio>
+    <admiralty-radio name="grp" value="option2">Option 2</admiralty-radio>
+  </admiralty-radio-group>`,
+  args: { stack: true } };
+
+export const YesNo: Story = { render: args => html`
+  <h3 id="group-label-boolean">
+    Do you agree?
+  </h3>
+  <admiralty-radio-group display-vertical="true" value=${args.value}>
+    <admiralty-radio name="grp" value="true">Yes</admiralty-radio>
+    <admiralty-radio name="grp" value="false">No</admiralty-radio>
+  </admiralty-radio-group>`,
+  args: { value: false } };
