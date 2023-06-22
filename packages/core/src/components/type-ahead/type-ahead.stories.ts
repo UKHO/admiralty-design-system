07c22ba6b731e4dcab835c7f7a3aae819ee3b1b4
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2023-06-22 16:21:48 +0000

    Prettier formatting of Typeahead stories

--- packages/core/src/components/type-ahead/type-ahead.stories.ts
+++ packages/core/src/components/type-ahead/type-ahead.stories.ts
@@ -7,16 +7,16 @@ const meta: Meta = {
   title: 'Forms/Typeahead',
   parameters: {
     actions: {
-        handles: ['valueChanged', 'selectionChanged']
-    }
+      handles: ['valueChanged', 'selectionChanged'],
+    },
   },
   args: {
     label: 'Please Type',
     hint: '',
     value: '',
     placeholder: '',
-    resultsOnInitFocus: false
-  }
+    resultsOnInitFocus: false,
+  },
 };
 
 export default meta;
@@ -24,49 +24,53 @@ export default meta;
 type Story = StoryObj<TypeAheadComponent>;
 
 const template: Story = {
-  render: args => html`
-    <admiralty-type-ahead
-      value = "${args.value}"
-      label="${args.label}"
-      hint="${args.hint}"
-      ?results-on-init-focus="${args.resultsOnInitFocus}"
-      placeholder="${args.placeholder}">
-      <admiralty-type-ahead-item value="dog"></admiralty-type-ahead-item>
-      <admiralty-type-ahead-item value="cat"></admiralty-type-ahead-item>
-      <admiralty-type-ahead-item value="elephant"></admiralty-type-ahead-item>
-      <admiralty-type-ahead-item value="badger"></admiralty-type-ahead-item>
-      <admiralty-type-ahead-item value="donkey"></admiralty-type-ahead-item>
-      <admiralty-type-ahead-item value="horse"></admiralty-type-ahead-item>
-      <admiralty-type-ahead-item value="lion"></admiralty-type-ahead-item>
-      <admiralty-type-ahead-item value="ostrich"></admiralty-type-ahead-item>
-      <admiralty-type-ahead-item value="rabbit"></admiralty-type-ahead-item>
-      <admiralty-type-ahead-item value="tiger"></admiralty-type-ahead-item>
-      <admiralty-type-ahead-item value="guinea pig"></admiralty-type-ahead-item>
-    </admiralty-type-ahead>`,
+  render: args => html` <admiralty-type-ahead
+    value="${args.value}"
+    label="${args.label}"
+    hint="${args.hint}"
+    ?results-on-init-focus="${args.resultsOnInitFocus}"
+    placeholder="${args.placeholder}"
+  >
+    <admiralty-type-ahead-item value="dog"></admiralty-type-ahead-item>
+    <admiralty-type-ahead-item value="cat"></admiralty-type-ahead-item>
+    <admiralty-type-ahead-item value="elephant"></admiralty-type-ahead-item>
+    <admiralty-type-ahead-item value="badger"></admiralty-type-ahead-item>
+    <admiralty-type-ahead-item value="donkey"></admiralty-type-ahead-item>
+    <admiralty-type-ahead-item value="horse"></admiralty-type-ahead-item>
+    <admiralty-type-ahead-item value="lion"></admiralty-type-ahead-item>
+    <admiralty-type-ahead-item value="ostrich"></admiralty-type-ahead-item>
+    <admiralty-type-ahead-item value="rabbit"></admiralty-type-ahead-item>
+    <admiralty-type-ahead-item value="tiger"></admiralty-type-ahead-item>
+    <admiralty-type-ahead-item value="guinea pig"></admiralty-type-ahead-item>
+  </admiralty-type-ahead>`,
 };
 
 export const WithoutSelectionAction: Story = { ...template };
 
-export const Prefilled: Story = { ...template, 
-  args: { 
-    value: 'donke2y'
-  }
+export const Prefilled: Story = {
+  ...template,
+  args: {
+    value: 'donke2y',
+  },
 };
 
-export const ShowResultsOnInitFocus: Story = { ...template, 
-  args: { 
-    resultsOnInitFocus: true
-  }
+export const ShowResultsOnInitFocus: Story = {
+  ...template,
+  args: {
+    resultsOnInitFocus: true,
+  },
 };
 
-export const withPlaceholderText: Story = { ...template, 
-  args: { 
+export const withPlaceholderText: Story = {
+  ...template,
+  args: {
     placeholder: 'Start typing the name of an animal',
-  }
+  },
 };
 
-export const withHint: Story = { ...template, 
-  args: { 
-    hint: 'Please type the name of an animal'
-  }
+export const withHint: Story = {
+  ...template,
+  args: {
+    hint: 'Please type the name of an animal',
+  },
 };
