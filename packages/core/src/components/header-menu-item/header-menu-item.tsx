f9ed310491e4f60dc246a446226a5c93cc05f3ff
Martin Davies <martin.davies@bjss.com>
2023-06-23 19:24:00 +0100

    used classes to control submenu visibility on desktop

--- packages/core/src/components/header-menu-item/header-menu-item.tsx
+++ packages/core/src/components/header-menu-item/header-menu-item.tsx
@@ -38,10 +38,50 @@ export class HeaderMenuItemComponent {
 
       if (this.subMenuExists) {
         this.subMenuSelector.setAttribute('aria-hidden', 'true');
+        //this.subMenuSelector.addEventListener('focusout', ev => this.handleFocusOut(ev));
       }
     } catch (e) {
       this.subMenuExists = false;
     }
+  }
+
+  handleFocusOut(ev: FocusEvent) {
+    const relatedTarget: any = ev?.relatedTarget;
+
+    // if (relatedTarget) {
+    //   console.log('handleFocusOut relatedTarget', ev.relatedTarget);
+    //   if ((ev.relatedTarget as any).outerHTML) {
+    //     console.log('handleFocusOut outerHTML', (ev.relatedTarget as any).outerHTML);
+    //   }
+
+    if (relatedTarget?.outerHTML?.indexOf('button') !== -1) {
+      this.closeSubMenu();
+    }
+    // } else {
+    //   console.log('handleFocusOut - no relatedTarget');
+    // }
+  }
+
+  private toggleSubMenu() {
+    const subMenu: HTMLDivElement = this.el.querySelector('div.sub-menu');
+    console.log('clicked-subMenuExists, display:', subMenu.style.display);
+    //subMenu.style.display = subMenu.style.display === 'flex' ? 'none' : 'flex';
+
+    if (subMenu.classList.contains('desktop-hide')) {
+      subMenu.classList.add('desktop-visible');
+      subMenu.classList.remove('desktop-hide');
+    } else {
+      subMenu.classList.add('desktop-hide');
+      subMenu.classList.remove('desktop-visible');
+
+    }
+  }
+
+  private closeSubMenu() {
+    const subMenu: HTMLDivElement = this.el.querySelector('div.sub-menu');
+    //console.log('handleFocusOut closing subMenu, display currently:', subMenu.style.display);
+    //subMenu.style.display = 'none';
+    subMenu.classList.add('desktop-hide');
   }
 
   handleClick(ev: MouseEvent): CustomEvent<void> {
@@ -51,12 +91,11 @@ export class HeaderMenuItemComponent {
     if (this.subMenuExists) {
       this.subMenuSelector.setAttribute('aria-hidden', 'false');
 
-      const subMenu: HTMLDivElement = this.el.querySelector('div.sub-menu');
-      console.log('clicked-subMenuExists, display:', subMenu.style.display);
-      subMenu.style.display = subMenu.style.display === 'flex' ? 'none' : 'flex';
+      this.toggleSubMenu();
     }
     return this.menuItemClick.emit();
   }
+
 
   /**
    * Handles mouseover events, used to set the aria hidden attribute when submenu
@@ -66,9 +105,7 @@ export class HeaderMenuItemComponent {
     if (this.subMenuExists) {
       this.subMenuSelector.setAttribute('aria-hidden', 'false');
 
-      const subMenu: HTMLDivElement = this.el.querySelector('div.sub-menu');
-      console.log('clicked-subMenuExists, display:', subMenu.style.display);
-      subMenu.style.display = subMenu.style.display === 'flex' ? 'none' : 'flex';
+      this.toggleSubMenu();
     }
   }
 
@@ -76,12 +113,14 @@ export class HeaderMenuItemComponent {
    * Handles mouseover events, used to set the aria hidden attribute when submenu
    * is no longer visible
    */
-  handleMouseOut(): void {
+  handleMouseOut(_ev: Event): void {
     if (this.subMenuExists) {
+      //ev.stopPropagation();
       this.subMenuSelector.setAttribute('aria-hidden', 'true');
-      const subMenu: HTMLDivElement = this.el.querySelector('div.sub-menu');
-      console.log('handleMouseOut clicked-subMenuExists, display:', subMenu.style.display);
-      subMenu.style.display = 'none';
+      //const subMenu: HTMLDivElement = this.el.querySelector('div.sub-menu');
+      //console.log('handleMouseOut subMenuExists, display:', subMenu.style.display);
+      //subMenu.style.display = 'none';
+      this.closeSubMenu();
     }
   }
 
@@ -91,12 +130,12 @@ export class HeaderMenuItemComponent {
     menuClass += active ? 'active' : '';
 
     return (
-      <Host onMouseOver={_ => this.handleMouseOver()} onMouseOut={_ => this.handleMouseOut()}>
+      <Host onMouseOver={_ => this.handleMouseOver()} onMouseOut={ev => this.handleMouseOut(ev)}>
         <div class={menuClass} onClick={ev => this.handleClick(ev)}>
           <button class="menu-title" tabindex="0">
             {menuTitle}
           </button>
-          <div class="sub-menu">
+          <div class="sub-menu desktop-hide" onFocusout={ev => this.handleFocusOut(ev)}>
             <slot />
           </div>
         </div>
