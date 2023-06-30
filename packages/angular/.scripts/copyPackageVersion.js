a682211389e03c97c9bf2c5ea83e67c99b770b7f
Tom Barham 🦄 <24593464+ecstaticrainbow@users.noreply.github.com>
2023-06-30 16:53:25 +0100

    switching the copy script to copy the exports from dist to root

--- packages/angular/.scripts/copyPackageVersion.js
+++ packages/angular/.scripts/copyPackageVersion.js
@@ -3,10 +3,6 @@ const rootPackageJson = require("../package.json");
 const distPackageJson = require("../dist/package.json");
 
 // Setting the package version as lerna doesn't do it for Angular packages
-distPackageJson.version = rootPackageJson.version;
+rootPackageJson.exports = distPackageJson.exports;
 
-// Updating the core dependency as lerna doesn't do it for Angular packages
-distPackageJson.dependencies["@ukho/admiralty-core"].version =
-  rootPackageJson.dependencies["@ukho/admiralty-core"].version;
-
-fs.writeFileSync("./dist/package.json", JSON.stringify(distPackageJson, null, 2));
+fs.writeFileSync("./package.json", JSON.stringify(rootPackageJson, null, 2));
