4d0f71d5bf8f7fd93a90aa141ef397cc7cee8af9
Tom Barham 🦄 <24593464+ecstaticrainbow@users.noreply.github.com>
2023-06-30 16:26:08 +0100

    resorting to running a custom script to update the angular package versions

--- packages/angular/.scripts/copyPackageVersion.js
+++ packages/angular/.scripts/copyPackageVersion.js
@@ -1,7 +1,12 @@
-const fs = require('fs');
-const rootPackageJson = require('../package.json');
-const distPackageJson = require('../dist/package.json')
+const fs = require("fs");
+const rootPackageJson = require("../package.json");
+const distPackageJson = require("../dist/package.json");
 
+// Setting the package version as lerna doesn't do it for Angular packages
 distPackageJson.version = rootPackageJson.version;
 
-fs.writeFileSync('./dist/package.json', JSON.stringify(distPackageJson,null, 2))
+// Updating the core dependency as lerna doesn't do it for Angular packages
+distPackageJson.dependencies["@ukho/admiralty-core"].version =
+  rootPackageJson.dependencies["@ukho/admiralty-core"].version;
+
+fs.writeFileSync("./dist/package.json", JSON.stringify(distPackageJson, null, 2));
