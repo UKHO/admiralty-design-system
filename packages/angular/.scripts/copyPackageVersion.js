89a27567338dc610e8997e1fce1b1499fe6e3d04
Tom Barham 🦄 <24593464+ecstaticrainbow@users.noreply.github.com>
2023-06-30 17:24:57 +0100

    Trying to add the other files to the core artifact

--- packages/angular/.scripts/copyPackageVersion.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const rootPackageJson = require("../package.json");
-const distPackageJson = require("../dist/package.json");
-
-// Setting the package version as lerna doesn't do it for Angular packages
-rootPackageJson.exports = distPackageJson.exports;
-
-fs.writeFileSync("./package.json", JSON.stringify(rootPackageJson, null, 2));
