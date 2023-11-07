bec8eaf9100949cb344609d2de465bdfa35b2be7
Tom Barham 🦄 <24593464+ecstaticrainbow@users.noreply.github.com>
2023-11-07 15:46:40 +0000

    Initial start on the design system homepage

--- packages/website/src/app/layout.tsx
+++ packages/website/src/app/layout.tsx
@@ -1,14 +1,23 @@
 "use client";
 import "@ukho/admiralty-core/styles/core.css";
 import "./globals.css";
-import { AdmiraltyHeader } from "@ukho/admiralty-react";
+import { AdmiraltyFooter, AdmiraltyHeader, AdmiraltyLink, AdmiraltyPhaseBanner } from "@ukho/admiralty-react";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         <AdmiraltyHeader headerTitle="Design System" logoImgUrl="/svg/Admiralty stacked logo.svg"></AdmiraltyHeader>
-        {children}
+        <AdmiraltyPhaseBanner
+          phase="alpha"
+          link="https://github.com/UKHO/admiralty-design-system/issues/new/choose"></AdmiraltyPhaseBanner>
+        <main>{children}</main>
+        <AdmiraltyFooter>
+          <AdmiraltyLink href="http://www.example.com" new-tab="true">
+            Privacy Policy
+          </AdmiraltyLink>
+          <AdmiraltyLink href="http://www.example.com">Accessibility</AdmiraltyLink>
+        </AdmiraltyFooter>
       </body>
     </html>
   );
--- packages/website/src/app/page.tsx
+++ packages/website/src/app/page.tsx
@@ -1,6 +1,17 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import HomePage from "./home.mdx";
+import "./home.css";
+import ImageBanner from "@/components/image-banner/image-banner";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <div>
+      <div className="hero-image">
+        <ImageBanner />
+      </div>
+      <div className="content-container">
+        <HomePage />
+      </div>
+    </div>
+  );
 }
