2b0c1e671c82b94584293e18d9820a307e2554a7
Tom Barham 🦄 <24593464+ecstaticrainbow@users.noreply.github.com>
2023-11-08 11:49:13 +0000

    fixing the horizontal scroll on the phase banner

--- packages/website/src/app/components/layout.tsx
+++ packages/website/src/app/components/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { AdmiraltySideNav, AdmiraltySideNavItem } from "@ukho/admiralty-react";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./docs.css";
 import { AdmiraltySideNavItemCustomEvent } from "@ukho/admiralty-core";
 import { useRouter } from "next/navigation";
@@ -9,13 +9,20 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const sideNavRef = useRef<HTMLAdmiraltySideNavElement>(null);
   const router = useRouter();
 
+  const [activeSideNavId, setactiveSideNavId] = useState<string>("");
+  useEffect(() => {
+    if (activeSideNavId) {
+      const sideNavItems = sideNavRef.current?.querySelectorAll("admiralty-side-nav-item");
+      sideNavItems?.forEach((sideNavItem) => {
+        sideNavItem.navActive = sideNavItem.sideNavItemId === activeSideNavId;
+        router.push(`/components/${activeSideNavId}`);
+      });
+    }
+  }, [activeSideNavId, router]);
+
   const onSideNavItemSelected = (event: AdmiraltySideNavItemCustomEvent<string>) => {
     const sideNavItemId: string = event.detail;
-    const sideNavItems = sideNavRef.current?.querySelectorAll("admiralty-side-nav-item");
-    sideNavItems?.forEach((sideNavItem) => {
-      sideNavItem.navActive = sideNavItem.sideNavItemId === sideNavItemId;
-      router.push(sideNavItemId);
-    });
+    setactiveSideNavId(sideNavItemId);
   };
 
   return (
@@ -24,7 +31,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <AdmiraltySideNav ref={sideNavRef}>
           <AdmiraltySideNavItem
             onSideNavItemSelected={onSideNavItemSelected}
-            navActive={true}
+            sideNavItemId="/"
+            headingTitle="Overview"></AdmiraltySideNavItem>
+          <AdmiraltySideNavItem
+            onSideNavItemSelected={onSideNavItemSelected}
             sideNavItemId="button"
             headingTitle="Button"></AdmiraltySideNavItem>
           <AdmiraltySideNavItem
--- packages/website/src/app/layout.tsx
+++ packages/website/src/app/layout.tsx
@@ -1,10 +1,17 @@
 "use client";
 import "@ukho/admiralty-core/styles/core.css";
 import "./globals.css";
-import { AdmiraltyFooter, AdmiraltyHeader, AdmiraltyLink, AdmiraltyPhaseBanner } from "@ukho/admiralty-react";
+import {
+  AdmiraltyFooter,
+  AdmiraltyHeader,
+  AdmiraltyHeaderMenuItem,
+  AdmiraltyLink,
+  AdmiraltyPhaseBanner
+} from "@ukho/admiralty-react";
 import Konami from "react-konami-code";
 import Snowfall from "react-snowfall";
 import { useEffect, useState } from "react";
+import {useRouter} from "next/navigation";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const [images, setImages] = useState(Array<HTMLImageElement>);
@@ -18,6 +25,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
     setImages([snowflake1, snowflake2, snowflake3]);
   },[]);
+
+  const router = useRouter();
 
   return (
     <html lang="en">
@@ -35,10 +44,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             }}></Snowfall>
         </Konami>
 
-        <AdmiraltyHeader headerTitle="Design System" logoImgUrl="/svg/Admiralty stacked logo.svg"></AdmiraltyHeader>
+        <AdmiraltyHeader headerTitle="Design System" onTitledClicked={() => router.push("/")} logoImgUrl="/svg/Admiralty stacked logo.svg" >
+          <AdmiraltyHeaderMenuItem
+              menuTitle="Components"
+              slot="items"
+              onMenuItemClick={() => router.push("/components")}></AdmiraltyHeaderMenuItem>
+        </AdmiraltyHeader>
+        <div>
         <AdmiraltyPhaseBanner
           phase="alpha"
           link="https://github.com/UKHO/admiralty-design-system/issues/new/choose"></AdmiraltyPhaseBanner>
+        </div>
         <main>{children}</main>
         <AdmiraltyFooter imageSrc="/svg/UKHO stacked logo.svg">
           <AdmiraltyLink href="http://www.example.com" new-tab="true">
