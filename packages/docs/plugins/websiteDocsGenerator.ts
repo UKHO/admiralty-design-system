28179e698df87715b1eb54c32077d504177b5100
Tom Barham 🦄 <24593464+ecstaticrainbow@users.noreply.github.com>
2023-11-10 13:56:59 +0000

    Updating the other files to ensure LF throughout the repo

--- packages/docs/plugins/websiteDocsGenerator.ts
+++ packages/docs/plugins/websiteDocsGenerator.ts
@@ -153,7 +153,7 @@ ${slots.map((slot) => `| \`${slot.name}\` | ${formatMultiline(slot.docs)} |`).jo
  * @returns The formatted string
  */
 const formatMultiline = (str: string) => {
-  return str.split('\r\n').join('<br /><br />').split('\n').join(' ');
+  return str.split('\n').join('<br /><br />').split('\n').join(' ');
 };
 
 const formatType = (attr: string, type: string) => {
