96afb0f2c53d0cce3e8a1e2a7a3a14fa2d007d91
Tom Barham 🦄 <24593464+ecstaticrainbow@users.noreply.github.com>
2023-11-28 16:29:07 +0000

    Add 'Brand Guide' section with color and typographic specifications.
    Added a new section to the website, 'Brand Guide', to provide a consolidated reference for brand attributes like color palettes and typography. This caters to the need for maintaining brand consistency across different elements of the website. Furthermore, added 'ColourPaletteItem' component to effectively display color palettes with specified Hex codes. Removed redundant '#' attribute in multiple CSS files to clean up the overall CSS.

---
+++ packages/website/src/app/brand-guide/images/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import ImagesPage from "./images.mdx";
+
+export default function Page() {
+  return <ImagesPage></ImagesPage>;
+}
---
+++ packages/website/src/app/brand-guide/logos/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import LogosPage from "./logos.mdx";
+
+export default function Page() {
+  return <LogosPage></LogosPage>;
+}
