777e43159619bc9fffe790a91023a24b6da732f2
Tom Barham 🦄 <24593464+ecstaticrainbow@users.noreply.github.com>
2023-12-04 16:36:48 +0000

    Adding the text area examples

---
+++ packages/website/src/usage/text-area/basic/demo.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { AdmiraltyTextarea } from "@ukho/admiralty-react";
+
+export default function Demo() {
+  return <AdmiraltyTextarea label="text"></AdmiraltyTextarea>;
+}
---
+++ packages/website/src/usage/text-area/with-text/demo.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { AdmiraltyTextarea } from "@ukho/admiralty-react";
+
+export default function Demo() {
+  return <AdmiraltyTextarea value="This is a text area" label="text"></AdmiraltyTextarea>;
+}
