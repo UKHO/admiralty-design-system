99559a810eec1b5693fe19f9825e1965feab2d2e
Tom Barham 🦄 <24593464+ecstaticrainbow@users.noreply.github.com>
2023-12-05 14:55:18 +0000

    Added select example

---
+++ packages/website/src/usage/select/basic/demo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { AdmiraltySelect } from "@ukho/admiralty-react";
+
+export default function Demo() {
+  return (
+    <AdmiraltySelect label="Do you like the design system?">
+      <option>Yes</option>
+      <option>No</option>
+    </AdmiraltySelect>
+  );
+}
