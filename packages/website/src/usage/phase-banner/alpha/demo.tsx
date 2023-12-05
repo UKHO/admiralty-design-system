6a618acf2dbd80b15d0351089f6db2db528b91f6
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2023-12-05 15:07:24 +0000

    Add Phase Banner example

---
+++ packages/website/src/usage/phase-banner/alpha/demo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { AdmiraltyPhaseBanner } from "@ukho/admiralty-react";
+
+export default function Demo() {
+  return (
+    <AdmiraltyPhaseBanner phase="alpha" link="https://example.org">
+    </AdmiraltyPhaseBanner>
+  );
+}
