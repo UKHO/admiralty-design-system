7c7d6bbd04b5ba5b5346155e7afd3a0b64d37392
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2023-12-05 10:20:55 +0000

    Fix dialogue examples

--- packages/website/src/usage/dialogue/error/demo.tsx
+++ packages/website/src/usage/dialogue/error/demo.tsx
@@ -3,7 +3,7 @@ import { AdmiraltyDialogue } from "@ukho/admiralty-react";
 
 export default function Demo() {
   return (
-    <AdmiraltyDialogue type="error" heading="There's a problem">
+    <AdmiraltyDialogue type="error" heading="There&apos;s a problem">
       It looks like there might be a problem with our system. Please try again in a few minutes.
     </AdmiraltyDialogue>
   );
--- packages/website/src/usage/dialogue/warning/demo.tsx
+++ packages/website/src/usage/dialogue/warning/demo.tsx
@@ -4,7 +4,7 @@ import { AdmiraltyDialogue } from "@ukho/admiralty-react";
 export default function Demo() {
   return (
     <AdmiraltyDialogue type="warning" heading="Check your application before submitting">
-      Once you've submitted the details of your application you won't be able to go back and change anything.
+      Once you&apos;ve submitted the details of your application you won&apos;t be able to go back and change anything.
     </AdmiraltyDialogue>
   );
 }
