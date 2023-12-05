8902242641738e51d6686d3a154bf73d54edba17
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2023-12-05 12:00:20 +0000

    Add header example

---
+++ packages/website/src/usage/header/basic/demo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { AdmiraltyHeader } from "@ukho/admiralty-react";
+
+export default function Demo() {
+  return (
+    <AdmiraltyHeader headerTitle="Design System" logoImgUrl="/svg/Admiralty stacked logo.svg">
+    </AdmiraltyHeader>
+  );
+}
