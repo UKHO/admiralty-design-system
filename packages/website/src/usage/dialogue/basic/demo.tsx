4b8dd46af40cbc1dea804d09d38b92cce2ada6db
lewis stevens <lewis.stevens1998@hotmail.com>
2024-01-29 13:42:19 +0000

    Dialogue - Basic example first attempt

---
+++ packages/website/src/usage/dialogue/basic/demo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { AdmiraltyDialogue } from "@ukho/admiralty-react";
+export default function Demo() {
+  return <AdmiraltyDialogue type="info" heading="Basic Title">
+    Some content in the dialogue
+  </AdmiraltyDialogue>;
+}
