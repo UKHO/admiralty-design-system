935947372d90bd9fd0fdb256f51a1cdd507f2c99
lewis stevens <lewis.stevens1998@hotmail.com>
2024-02-02 14:27:13 +0000

    Header - Signed In

---
+++ packages/website/src/usage/header/signed-in/demo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AdmiraltyHeader, AdmiraltyHeaderProfile } from "@ukho/admiralty-react";
+
+export default function Demo() {
+  return (
+    <AdmiraltyHeader headerTitle="Design System" logoImgUrl="/svg/Admiralty stacked logo.svg">
+      <AdmiraltyHeaderProfile slot="profile" is-signed-in="true" signed-in-text="Mr Admiral" sign-in-only="false"></AdmiraltyHeaderProfile>
+    </AdmiraltyHeader>
+  );
+}
