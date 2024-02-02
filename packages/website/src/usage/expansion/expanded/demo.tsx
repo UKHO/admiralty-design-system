a6d5ed8980112b2cd657aedde022f59824b02607
lewis stevens <lewis.stevens1998@hotmail.com>
2024-02-02 15:04:15 +0000

    Fixed TS syntax

--- packages/website/src/usage/expansion/expanded/demo.tsx
+++ packages/website/src/usage/expansion/expanded/demo.tsx
@@ -3,7 +3,7 @@ import { AdmiraltyExpansion } from "@ukho/admiralty-react";
 
 export default function Demo() {
   return (
-    <AdmiraltyExpansion heading="Expansion Heading" expanded="">
+    <AdmiraltyExpansion heading="Expansion Heading" expanded={true}>
       Some content for the expansion
     </AdmiraltyExpansion>
   );
