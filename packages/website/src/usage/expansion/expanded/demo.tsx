0b07c717bee996796bd13490f3a2144253b0710b
lewis stevens <lewis.stevens1998@hotmail.com>
2024-02-15 10:13:02 +0000

    WIP - Fixing Expansion issues

--- packages/website/src/usage/expansion/expanded/demo.tsx
+++ packages/website/src/usage/expansion/expanded/demo.tsx
@@ -3,7 +3,7 @@ import { AdmiraltyExpansion } from "@ukho/admiralty-react";
 
 export default function Demo() {
   return (
-    <AdmiraltyExpansion heading="Expansion Heading" expanded={true}>
+    <AdmiraltyExpansion heading="Expansion Heading" align-heading-right={true}>
       Some content for the expansion
     </AdmiraltyExpansion>
   );
--- packages/website/src/usage/expansion/no-border/demo.tsx
+++ packages/website/src/usage/expansion/no-border/demo.tsx
@@ -3,15 +3,16 @@ import { AdmiraltyExpansion } from "@ukho/admiralty-react";
 
 export default function Demo() {
   return (
-    <AdmiraltyExpansion>
-      <AdmiraltyExpansion heading="Expansion Heading" expanded={false} align-heading-right="false" hide-border="true">
-        Some content for the expansion with no border
-      </AdmiraltyExpansion>
-      <AdmiraltyExpansion heading="Expansion Heading" expanded={false} align-heading-right="false" hide-border="true">
-        Some content for the expansion with no border
-      </AdmiraltyExpansion>
-      <AdmiraltyExpansion heading="Expansion Heading" expanded={false} align-heading-right="false" hide-border="true">
-        Some content for the expansion with no border
-      </AdmiraltyExpansion>
-    </AdmiraltyExpansion>);
+    <React.Fragment>
+    <AdmiraltyExpansion heading="Expansion Heading 1" expanded={false} align-heading-right={false} hide-border={true}>
+      Some content for the expansion with no border
+    </AdmiraltyExpansion>
+    <AdmiraltyExpansion heading="Expansion Heading 2" expanded={false} align-heading-right={false} hide-border={true}>
+      Some content for the expansion with no border
+    </AdmiraltyExpansion>
+    <AdmiraltyExpansion heading="Expansion Heading 3" expanded={false} align-heading-right={false} hide-border={true}>
+      Some content for the expansion with no border
+    </AdmiraltyExpansion>
+    </React.Fragment>
+  );
 }
