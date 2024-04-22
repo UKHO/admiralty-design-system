ed6bbeccfc842bd44f6b0a4178ffdd71b5a5c922
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2024-04-22 11:52:48 +0000

    Style component from SCSS rather than inline

--- packages/core/src/components/modal-dialog/modal-dialog.stories.ts
+++ packages/core/src/components/modal-dialog/modal-dialog.stories.ts
@@ -14,31 +14,15 @@ export default meta;
 
 type Story = StoryObj<ModalDialogComponent>;
 
-export const WithTitle: Story = {
-  render: args => html` <admiralty-modal-dialog heading="${args.heading}"><div slot="content">Some content</div></admiralty-modal-dialog>`,
-  args: { heading: 'I am a card' },
-};
-
-export const WithoutTitle: Story = {
-  render: args => html` <admiralty-modal-dialog><div slot="content">Some content</div></admiralty-modal-dialog>`,
-};
-
-export const WithHtmlContent: Story = {
-  render: args => html` <admiralty-modal-dialog heading="${args.heading}"
-    ><div slot="content">You can put any html content in the body of a card. Such as <a href="#">links</a> or <b>bold text</b></div></admiralty-modal-dialog
-  >`,
-  args: { heading: 'I am a card' },
-};
-
-export const WithActions: Story = {
+export const Basic: Story = {
   render: args => html`<admiralty-modal-dialog heading="${args.heading}">
-    <div slot="content" style="display: flex; align-items: start;">
-      <admiralty-icon icon-name="triangle-exclamation" icon-prefix="fas" style="color: #09315B; display: inline-block; padding: 5px;"></admiralty-icon>
-      <div style="color: #09315B; max-width: 500px; display: inline-block">If you leave this page, your survey won't be saved and can't be recovered</div>
+    <div slot="content">
+      <admiralty-icon icon-name="triangle-exclamation" icon-prefix="fas"></admiralty-icon>
+      <div>If you leave this page, your survey won't be saved and can't be recovered</div>
     </div>
-    <div slot="actions" style="display: flex; justify-content: end;">
-      <admiralty-button variant="secondary" style="padding: 0px 8px;">Leave page</admiralty-button>
-      <admiralty-button style="padding: 0px 8px;">Continue survey</admiralty-button>
+    <div slot="actions">
+      <admiralty-button variant="secondary">Leave page</admiralty-button>
+      <admiralty-button>Continue survey</admiralty-button>
     </div>
   </admiralty-modal-dialog>`,
   args: { heading: 'Do you want to leave this page?' },
