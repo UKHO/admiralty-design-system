fe664c73acc7f8224c301b8473b619994edef363
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2024-04-25 17:30:04 +0000

    Fix and add Modal Dialog tests

--- packages/core/src/components/modal-dialog/modal-dialog.spec.ts
+++ packages/core/src/components/modal-dialog/modal-dialog.spec.ts
@@ -9,9 +9,68 @@ describe('admiralty-modal-dialog', () => {
     });
     expect(root).toEqualHtml(`
       <admiralty-modal-dialog>
-        <div class="modal-dialog">
-          <div class="modal-content"></div>
-          <div class="modal-actions" role="navigation"></div>
+        <div>
+          <div class="modal-dialog">
+            <div class="modal-content"></div>
+            <div class="modal-actions" role="navigation"></div>
+          </div>
+          <div class="modal-backdrop">
+          </div>
+        </div>
+      </admiralty-modal-dialog>
+    `);
+  });
+  it('renders with heading when heading is passed', async () => {
+    const { root } = await newSpecPage({
+      components: [ModalDialogComponent],
+      html: '<admiralty-modal-dialog heading="TESTHEADING"></admiralty-modal-dialog>',
+    });
+    expect(root).toEqualHtml(`
+      <admiralty-modal-dialog heading="TESTHEADING">
+        <div>
+          <div class="modal-dialog">
+            <p class="modal-heading">TESTHEADING</p>
+            <div class="modal-content"></div>
+            <div class="modal-actions" role="navigation"></div>
+          </div>
+          <div class="modal-backdrop">
+          </div>
+        </div>
+      </admiralty-modal-dialog>
+    `);
+  });
+  it('renders with show classes when show is true', async () => {
+    const { root } = await newSpecPage({
+      components: [ModalDialogComponent],
+      html: '<admiralty-modal-dialog show="true"></admiralty-modal-dialog>',
+    });
+    expect(root).toEqualHtml(`
+      <admiralty-modal-dialog show="true">
+        <div>
+          <div class="modal-dialog show">
+            <div class="modal-content"></div>
+            <div class="modal-actions" role="navigation"></div>
+          </div>
+          <div class="modal-backdrop show">
+          </div>
+        </div>
+      </admiralty-modal-dialog>
+    `);
+  });
+  it('renders without show classes when show is false', async () => {
+    const { root } = await newSpecPage({
+      components: [ModalDialogComponent],
+      html: '<admiralty-modal-dialog show="false"></admiralty-modal-dialog>',
+    });
+    expect(root).toEqualHtml(`
+      <admiralty-modal-dialog show="false">
+        <div>
+          <div class="modal-dialog">
+            <div class="modal-content"></div>
+            <div class="modal-actions" role="navigation"></div>
+          </div>
+          <div class="modal-backdrop">
+          </div>
         </div>
       </admiralty-modal-dialog>
     `);
