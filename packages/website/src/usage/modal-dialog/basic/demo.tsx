37a3049bc14b2d6db1cf6548761ab8707a0a81ee
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2024-05-22 14:05:14 +0000

    Fix Modal Dialog docs example to only show the Modal Dialog on button click
    Co-authored-by: TB <24593464+ecstaticrainbow@users.noreply.github.com>

--- packages/website/src/usage/modal-dialog/basic/demo.tsx
+++ packages/website/src/usage/modal-dialog/basic/demo.tsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { AdmiraltyModalDialog, AdmiraltyIcon, AdmiraltyButton } from "@ukho/admiralty-react";
 
 export default function Demo() {
+  const [modalDialogShow, setModalDialogShow] = useState(false);
+  function showModalDialog() {
+    setModalDialogShow(true);
+  }
+  function hideModalDialog() {
+    setModalDialogShow(false);
+  }
   return (
-    <AdmiraltyModalDialog
-      heading="Do you want to leave this page?"
-      show={true}
-      label="Do you want to leave this page?"
-      description="If you leave this page, your survey won't be saved and can't be recovered">
-      <div slot="content">
-        <AdmiraltyIcon icon-name="triangle-exclamation" icon-prefix="fas"></AdmiraltyIcon>
-        <div>If you leave this page, your survey won&apos;t be saved and can&apos;t be recovered</div>
-      </div>
-      <div slot="actions">
-        <AdmiraltyButton variant="secondary">Leave page</AdmiraltyButton>
-        <AdmiraltyButton>Continue survey</AdmiraltyButton>
-      </div>
-    </AdmiraltyModalDialog>
+    <div>
+      <AdmiraltyButton onClick={showModalDialog}>Show Modal Dialog</AdmiraltyButton>
+      <AdmiraltyModalDialog
+        heading="Do you want to leave this page?"
+        show={modalDialogShow}
+        label="Do you want to leave this page?"
+        description="If you leave this page, your survey won't be saved and can't be recovered">
+        <div slot="content">
+          <AdmiraltyIcon icon-name="triangle-exclamation" icon-prefix="fas"></AdmiraltyIcon>
+          <div>If you leave this page, your survey won&apos;t be saved and can&apos;t be recovered</div>
+        </div>
+        <div slot="actions">
+          <AdmiraltyButton variant="secondary" onClick={hideModalDialog}>
+            Leave page
+          </AdmiraltyButton>
+          <AdmiraltyButton onClick={hideModalDialog}>Continue survey</AdmiraltyButton>
+        </div>
+      </AdmiraltyModalDialog>
+    </div>
   );
 }
