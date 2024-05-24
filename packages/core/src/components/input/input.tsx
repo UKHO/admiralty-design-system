158b5c6b05a375a751903feb1b3bcc60ddc885ed
Tom Barham <24593464+ecstaticrainbow@users.noreply.github.com>
2024-05-24 14:06:14 +0100

    Adding in the ability to pass in options to the autocomplete

--- packages/core/src/components/input/input.tsx
+++ packages/core/src/components/input/input.tsx
@@ -159,7 +159,9 @@ export class InputComponent implements ComponentInterface {
             maxWidth: this.width ? `${this.width}px` : null,
           }}
         />
-        <admiralty-input-invalid style={{ visibility: this.invalid && this.invalidMessage ? 'visible' : 'hidden' }}>{this.invalidMessage}</admiralty-input-invalid>
+        {this.invalidMessage && this.invalid && (
+          <admiralty-input-invalid style={{ visibility: this.invalid && this.invalidMessage ? 'visible' : 'hidden' }}>{this.invalidMessage}</admiralty-input-invalid>
+        )}
       </div>
     );
   }
