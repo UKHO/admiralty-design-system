395d521f4a9e57e17b7dc7ba1f788f8ae45ab3cd
Natalie Johnson <natalie.johnson@bjss.com>
2024-08-01 13:03:26 +0100

    Change website to use updated header example

--- packages/website/src/usage/header/menu-item/demo.tsx
+++ packages/website/src/usage/header/menu-item/demo.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import {AdmiraltyHeader, AdmiraltyHeaderMenuItem, AdmiraltyHeaderSubMenuItem, AdmiraltyHeaderProfile} from "@ukho/admiralty-react";
+import {
+  AdmiraltyHeader,
+  AdmiraltyHeaderMenuItem,
+  AdmiraltyHeaderMenuLink,
+  AdmiraltyHeaderSubMenuItem,
+  AdmiraltyHeaderProfile,
+} from "@ukho/admiralty-react";
 
 export default function Demo() {
   return (
@@ -9,14 +15,18 @@ export default function Demo() {
         <AdmiraltyHeaderSubMenuItem menu-title="Super long sub navigation item for wrapping"></AdmiraltyHeaderSubMenuItem>
         <AdmiraltyHeaderSubMenuItem menu-title="sub item 3"></AdmiraltyHeaderSubMenuItem>
       </AdmiraltyHeaderMenuItem>
-      <AdmiraltyHeaderMenuItem menu-title="Components &amp; Patterns" active={false}  slot="items">
-        <AdmiraltyHeaderSubMenuItem menu-title="sub item 1"></AdmiraltyHeaderSubMenuItem>
-        <AdmiraltyHeaderSubMenuItem menu-title="sub item 2"></AdmiraltyHeaderSubMenuItem>
-      </AdmiraltyHeaderMenuItem>
-      <AdmiraltyHeaderMenuItem>
+      <AdmiraltyHeaderMenuLink
+        menu-title="Item 2"
+        active={false}
+        href="/components/header"
+        suppress-redirect={true}
+        slot="items"
+      />
+      <AdmiraltyHeaderMenuItem menu-title="Item 3" active={false} slot="items">
         <AdmiraltyHeaderSubMenuItem menu-title="sub item 1"></AdmiraltyHeaderSubMenuItem>
       </AdmiraltyHeaderMenuItem>
       <AdmiraltyHeaderProfile is-signed-in={false} signed-in-text="Mr Admiral" slot="profile"></AdmiraltyHeaderProfile>
     </AdmiraltyHeader>
   );
 }
+
