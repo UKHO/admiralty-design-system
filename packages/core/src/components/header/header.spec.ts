6265ec2ece2dab542bbb737bdc70f5254f6f3ca2
Natalie Johnson <natalie.johnson@bjss.com>
2024-09-17 13:31:22 +0100

    Update in DOM where nav is declared

--- packages/core/src/components/header/header.spec.ts
+++ packages/core/src/components/header/header.spec.ts
@@ -20,25 +20,25 @@ describe('admiralty-header', () => {
       <admiralty-header>
         <!---->
         <div class="admiralty-header">
-          <nav class="header-menu">
+          <div class="header-menu">
             <div class="header-branding">
               <a class="header-logo" href="https://www.admiralty.co.uk/" tabindex="0">
                 <img alt="ADMIRALTY" class="header-image" src="svg/Admiralty stacked logo.svg">
               </a>
               <div class="vertical-seperator"></div>
             </div>
-            <div class="header-menus">
+            <nav aria-label="Site navigation" class="header-menus" role="navigation">
               <div class="mobile-menu-toggle">
                 <button aria-label="Show menu">
                   <admiralty-icon icon-name="bars"></admiralty-icon>
                 </button>
               </div>
               <div class="menu-sections">
-                <div class="menu-items" role="navigation"></div>
-                <div class="header-profile" role="navigation"></div>
+                <div class="menu-items"></div>
+                <div class="header-profile"></div>
               </div>
-            </div>
-          </nav>
+            </nav>
+          </div>
         </div>
       </admiralty-header>
     `);
@@ -53,7 +53,7 @@ describe('admiralty-header', () => {
       <admiralty-header header-title="Design System">
         <!---->
         <div class="admiralty-header">
-          <nav class="header-menu">
+          <div class="header-menu">
             <div class="header-branding">
               <a class="header-logo" href="https://www.admiralty.co.uk/" tabindex="0">
                 <img alt="ADMIRALTY" class="header-image" src="svg/Admiralty stacked logo.svg">
@@ -65,18 +65,18 @@ describe('admiralty-header', () => {
                 </a>
               </h2>
             </div>
-            <div class="header-menus">
+            <nav aria-label="Site navigation" class="header-menus" role="navigation">
               <div class="mobile-menu-toggle">
                 <button aria-label="Show menu">
                   <admiralty-icon icon-name="bars"></admiralty-icon>
                 </button>
               </div>
               <div class="menu-sections">
-                <div class="menu-items" role="navigation"></div>
-                <div class="header-profile" role="navigation"></div>
+                <div class="menu-items"></div>
+                <div class="header-profile"></div>
               </div>
-            </div>
-          </nav>
+            </nav>
+          </div>
         </div>
       </admiralty-header>
     `);
@@ -91,7 +91,7 @@ describe('admiralty-header', () => {
       <admiralty-header header-title="Design System" header-title-url="null" logo-link-url="http://www.example.com">
         <!---->
         <div class="admiralty-header">
-          <nav class="header-menu">
+          <div class="header-menu">
             <div class="header-branding">
               <a class="header-logo" href="http://www.example.com" tabindex="0">
                 <img alt="ADMIRALTY" class="header-image" src="svg/Admiralty stacked logo.svg">
@@ -103,18 +103,18 @@ describe('admiralty-header', () => {
                 </a>
               </h2>
             </div>
-            <div class="header-menus">
+            <nav aria-label="Site navigation" class="header-menus" role="navigation">
               <div class="mobile-menu-toggle">
                 <button aria-label="Show menu">
                   <admiralty-icon icon-name="bars"></admiralty-icon>
                 </button>
               </div>
               <div class="menu-sections">
-                <div class="menu-items" role="navigation"></div>
-                <div class="header-profile" role="navigation"></div>
+                <div class="menu-items"></div>
+                <div class="header-profile"></div>
               </div>
-            </div>
-          </nav>
+            </nav>
+          </div>
         </div>
       </admiralty-header>
     `);
@@ -132,7 +132,7 @@ describe('admiralty-header', () => {
       <admiralty-header header-title="Design System" header-title-url="#" logo-alt-text="Logo" logo-img-url="logo.svg" logo-link-url="http://www.example.com">
         <!---->
         <div class="admiralty-header">
-          <nav class="header-menu">
+          <div class="header-menu">
             <div class="header-branding">
               <a class="header-logo" href="http://www.example.com" tabindex="0">
                 <img alt="Logo" class="header-image" src="logo.svg">
@@ -144,20 +144,20 @@ describe('admiralty-header', () => {
                 </a>
               </h2>
             </div>
-            <div class="header-menus">
+            <nav aria-label="Site navigation" class="header-menus" role="navigation">
               <div class="display-hamburger mobile-menu-toggle">
                 <button aria-label="Show menu">
                   <admiralty-icon icon-name="bars"></admiralty-icon>
                 </button>
               </div>
               <div class="menu-sections">
-                <div class="menu-items" role="navigation"></div>
-                <div class="header-profile" role="navigation">
+                <div class="menu-items"></div>
+                <div class="header-profile">
                   <admiralty-header-profile is-signed-in="true" signed-in-text="Mr Admiral" slot="profile"></admiralty-header-profile>
                 </div>
               </div>
-            </div>
-          </nav>
+            </nav>
+          </div>
         </div>
       </admiralty-header>
     `);
@@ -175,7 +175,7 @@ describe('admiralty-header', () => {
       <admiralty-header header-title="Design System" header-title-url="#" logo-alt-text="Logo" logo-img-url="logo.svg" logo-link-url="http://www.example.com">
         <!---->
         <div class="admiralty-header">
-          <nav class="header-menu">
+          <div class="header-menu">
             <div class="header-branding">
               <a class="header-logo" href="http://www.example.com" tabindex="0">
                 <img alt="Logo" class="header-image" src="logo.svg">
@@ -187,20 +187,20 @@ describe('admiralty-header', () => {
                 </a>
               </h2>
             </div>
-            <div class="header-menus">
+            <nav aria-label="Site navigation" class="header-menus" role="navigation">
               <div class="display-hamburger mobile-menu-toggle">
                 <button aria-label="Show menu">
                   <admiralty-icon icon-name="bars"></admiralty-icon>
                 </button>
               </div>
               <div class="menu-sections">
-                <div class="menu-items" role="navigation"></div>
-                <div class="header-profile" role="navigation">
+                <div class="menu-items"></div>
+                <div class="header-profile">
                   <admiralty-header-profile is-signed-in="false" signed-in-text="Mr Admiral" slot="profile"></admiralty-header-profile>
                 </div>
               </div>
-            </div>
-          </nav>
+            </nav>
+          </div>
         </div>
       </admiralty-header>
     `);
@@ -219,7 +219,7 @@ describe('admiralty-header', () => {
       <admiralty-header header-title="Design System" header-title-url="#" logo-alt-text="Logo" logo-img-url="logo.svg" logo-link-url="http://www.example.com">
         <!---->
         <div class="admiralty-header">
-          <nav class="header-menu">
+          <div class="header-menu">
             <div class="header-branding">
               <a class="header-logo" href="http://www.example.com" tabindex="0">
                 <img alt="Logo" class="header-image" src="logo.svg">
@@ -231,21 +231,21 @@ describe('admiralty-header', () => {
                 </a>
               </h2>
             </div>
-            <div class="header-menus">
+            <nav aria-label="Site navigation" class="header-menus" role="navigation">
               <div class="display-hamburger mobile-menu-toggle">
                 <button aria-label="Show menu">
                   <admiralty-icon icon-name="bars"></admiralty-icon>
                 </button>
               </div>
               <div class="menu-sections">
-                <div class="menu-items" role="navigation">
+                <div class="menu-items">
                   <admiralty-header-menu-item active="false" menu-title="Item 1" slot="items"></admiralty-header-menu-item>
                   <admiralty-header-menu-item active="false" menu-title="Item 2" slot="items"></admiralty-header-menu-item>
                 </div>
-                <div class="header-profile" role="navigation"></div>
+                <div class="header-profile"></div>
               </div>
-            </div>
-          </nav>
+            </nav>
+          </div>
         </div>
       </admiralty-header>
     `);
@@ -270,7 +270,7 @@ describe('admiralty-header', () => {
       <admiralty-header header-title="Design System" header-title-url="#" logo-alt-text="Logo" logo-img-url="logo.svg" logo-link-url="http://www.example.com">
         <!---->
         <div class="admiralty-header">
-          <nav class="header-menu">
+          <div class="header-menu">
             <div class="header-branding">
               <a class="header-logo" href="http://www.example.com" tabindex="0">
                 <img alt="Logo" class="header-image" src="logo.svg">
@@ -282,14 +282,14 @@ describe('admiralty-header', () => {
                 </a>
               </h2>
             </div>
-            <div class="header-menus">
+            <nav aria-label="Site navigation" class="header-menus" role="navigation">
               <div class="display-hamburger mobile-menu-toggle">
                 <button aria-label="Show menu">
                   <admiralty-icon icon-name="bars"></admiralty-icon>
                 </button>
               </div>
               <div class="menu-sections">
-                <div class="menu-items" role="navigation">
+                <div class="menu-items">
                   <admiralty-header-menu-item active="false" menu-title="Item 1" slot="items">
                     <admiralty-header-sub-menu-item menu-title="sub item 1"></admiralty-header-sub-menu-item>
                     <admiralty-header-sub-menu-item menu-title="sub item 2"></admiralty-header-sub-menu-item>
@@ -299,10 +299,10 @@ describe('admiralty-header', () => {
                     <admiralty-header-sub-menu-item menu-title="sub item 3"></admiralty-header-sub-menu-item>
                   </admiralty-header-menu-item>
                 </div>
-                <div class="header-profile" role="navigation"></div>
+                <div class="header-profile"></div>
               </div>
-            </div>
-          </nav>
+            </nav>
+          </div>
         </div>
       </admiralty-header>
     `);
