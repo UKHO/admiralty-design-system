2bcf245c26c7186d97248d7dee62ef63a72c0185
Tom Barham <24593464+ecstaticrainbow@users.noreply.github.com>
2024-09-20 16:40:05 +0100

    Fixing the nextjs build by adding a custom 404 page

--- packages/website/next.config.mjs
+++ packages/website/next.config.mjs
@@ -4,7 +4,9 @@ import rehypeSlug from 'rehype-slug';
 import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+};
 
 const withMDX = createMDX({
     // Add markdown plugins here, as desired
