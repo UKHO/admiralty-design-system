a9882a666c2238fe48da9bcf831f1afaa9af6f02
Natalie Johnson <natalie.johnson@bjss.com>
2024-09-27 15:46:02 +0100

    Update header to be more accessible

--- packages/core/src/components/header-sub-menu-item/header-sub-menu-item.tsx
+++ packages/core/src/components/header-sub-menu-item/header-sub-menu-item.tsx
@@ -13,15 +13,30 @@ export class HeaderSubMenuItemComponent {
   @Prop() menuTitle: string;
 
   /**
-   * The event that is fired when a user clicks on the menu.
+   * The URL to link to.
+   */
+  @Prop() href: string;
+
+  /**
+   * Causes the default browser redirect to be suppressed. Can be used in conjunction with the
+   * `onMenuItemClick` event to use a navigation router and prevent a full page reload when navigating.
+   */
+  @Prop() suppressRedirect?: boolean = false;
+
+  /**
+   * The event that is fired when a user clicks on the menu item.
    * Event contains the menu item text.
    */
   @Event() subMenuItemClick: EventEmitter<string>;
 
   handleClick(ev: MouseEvent): CustomEvent<string> {
+    if (this.suppressRedirect) {
+      ev.preventDefault();
+    }
     ev.stopPropagation();
+
     return this.subMenuItemClick.emit(this.menuTitle);
-  };
+  }
 
   onKeyDown(ev: KeyboardEvent): void {
     if (ev.key === Keys.ENTER || ev.key === Keys.SPACE) {
@@ -32,11 +47,19 @@ export class HeaderSubMenuItemComponent {
   render() {
     return (
       <Host>
-        <div class="header-sub-menu-item" onClick={ev=>this.handleClick(ev)} onKeyDown={ev=>this.onKeyDown(ev)} tabindex="0">
-          <span class="title">{this.menuTitle}</span>
-        </div>
+        {this.href ? (
+          <div class="header-sub-menu-item" onClick={ev => this.handleClick(ev)}>
+            <a class="title" href={this.href} onClick={ev => this.handleClick(ev)}>
+              {this.menuTitle}
+              <slot></slot>
+            </a>
+          </div>
+        ) : (
+          <div class="header-sub-menu-item" onClick={ev => this.handleClick(ev)} onKeyDown={ev => this.onKeyDown(ev)} tabindex="0">
+            <span class="title">{this.menuTitle}</span>
+          </div>
+        )}
       </Host>
     );
   }
-
 }
