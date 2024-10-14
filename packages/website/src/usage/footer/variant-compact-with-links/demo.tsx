817616bb1c6feb538bb8e5896d2492c0746e01ac
ENGINEERING\MistryN <24655342+developernm@users.noreply.github.com>
2024-10-14 15:37:15 +0100

    docs: updated docs to have light grey background

--- packages/website/src/usage/footer/variant-compact-with-links/demo.tsx
+++ packages/website/src/usage/footer/variant-compact-with-links/demo.tsx
@@ -3,9 +3,11 @@ import { AdmiraltyFooter, AdmiraltyLink } from "@ukho/admiralty-react";
 
 export default function Demo() {
   return (
-    <AdmiraltyFooter image-src="/svg/UKHO stacked logo.svg" variant="compact">
-      <AdmiraltyLink href="http://www.example.com" new-tab="true">Privacy Policy</AdmiraltyLink>
-      <AdmiraltyLink href="http://www.example.com">Accessibility</AdmiraltyLink>
-    </AdmiraltyFooter>
+    <div style={{ backgroundColor: "lightgrey", padding: '1rem' }}>
+      <AdmiraltyFooter image-src="/svg/UKHO stacked logo.svg" variant="compact">
+        <AdmiraltyLink href="http://www.example.com" new-tab="true">Privacy Policy</AdmiraltyLink>
+        <AdmiraltyLink href="http://www.example.com">Accessibility</AdmiraltyLink>
+      </AdmiraltyFooter>
+    </div>
   );
 }
--- packages/website/src/usage/footer/variant-compact/demo.tsx
+++ packages/website/src/usage/footer/variant-compact/demo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import { AdmiraltyFooter } from "@ukho/admiralty-react";
 export default function Demo() {
-  return <AdmiraltyFooter image-src="/svg/UKHO stacked logo.svg" variant="compact"></AdmiraltyFooter>;
+  return <div style={{ backgroundColor: "lightgrey", padding: '1rem' }}><AdmiraltyFooter image-src="/svg/UKHO stacked logo.svg" variant="compact"></AdmiraltyFooter></div>;
 }
