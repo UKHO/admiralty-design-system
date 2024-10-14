6ae869a69b2604b184e4f8b2bd3a1606c51548bd
ENGINEERING\MistryN <24655342+developernm@users.noreply.github.com>
2024-10-14 14:32:54 +0100

    docs: updated website documentation

---
+++ packages/website/src/usage/footer/variant-compact/demo.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { AdmiraltyFooter } from "@ukho/admiralty-react";
+export default function Demo() {
+  return <AdmiraltyFooter image-src="/svg/UKHO stacked logo.svg" variant="compact"></AdmiraltyFooter>;
+}
