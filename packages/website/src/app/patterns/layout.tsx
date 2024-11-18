d14ebe1ef5c37bc6256fba1f38b29260cfa8f82d
lewis stevens <lewis.stevens1998@hotmail.com>
2024-11-18 14:56:18 +0000

    Create patterns page with placeholder text and sidebar items

---
+++ packages/website/src/app/patterns/layout.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+import styles from "./styles.module.css";
+import SideNav, { SideNavItem } from "@/components/side-nav/side-nav";
+
+const sideNavItems: SideNavItem[] = [
+  { path: "/", title: "Overview", },
+  { path: "test1", title: "Test 1" },
+];
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={styles.contentContainer}>
+    <SideNav rootPath="/patterns" items={sideNavItems} />
+    <div>{children}</div>
+    </div>
+  );
+}
