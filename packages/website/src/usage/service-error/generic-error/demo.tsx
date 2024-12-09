5aad5a58d1be83c0644a664bac3bb1df9c115cf4
lewis stevens <lewis.stevens1998@hotmail.com>
2024-12-09 10:12:59 +0000

    Add service error: generic error example

---
+++ packages/website/src/usage/service-error/generic-error/demo.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { AdmiraltyHeader, AdmiraltyFooter, AdmiraltyLink } from "@ukho/admiralty-react";
+import {  } from "@ukho/admiralty-core/storybook-static/components/admiralty-header";
+
+
+export default function Demo() {
+  return (
+    <>
+      <AdmiraltyHeader headerTitle="Design System" logoImgUrl="/svg/UKHO linear logo.svg"></AdmiraltyHeader>
+      <div>
+        <h1>An error has occurred</h1>
+        <p>We apologise for the inconvenience, but an error has occurred.</p>
+        <p>
+          Please <a href="#"><strong>return to the homepage</strong></a> and try again. If the issue persists, please contanct <br/>
+          Customer Services for further assistance.
+        </p>
+        <p>
+          Email:
+          <a href="mailto:example.email@ukho.gov.uk">
+            <strong>customerservices@ukho.gov.uk</strong>
+          </a>
+          <br />
+          We aim to respond within two working days.
+        </p>
+      </div>
+      <AdmiraltyFooter imageSrc="/svg/UKHO stacked logo.svg">
+        <AdmiraltyLink href="http://www.example.com" new-tab="true">Privacy Policy</AdmiraltyLink>
+        <AdmiraltyLink href="http://www.example.com">Accessibility</AdmiraltyLink>
+      </AdmiraltyFooter>
+    </>
+  );
+}
+
