3c417b827488838d8e9bfdccc6295926904f83a3
lewis stevens <lewis.stevens1998@hotmail.com>
2025-01-09 14:15:37 +0000

    Update link wording on basic link example

--- packages/website/src/usage/link/basic/demo.tsx
+++ packages/website/src/usage/link/basic/demo.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { AdmiraltyLink } from "@ukho/admiralty-react";
 
 export default function Demo() {
-  return <AdmiraltyLink href="https://www.example.com">Go here</AdmiraltyLink>;
+  return <AdmiraltyLink href="https://www.example.com">Open Link</AdmiraltyLink>;
 }
