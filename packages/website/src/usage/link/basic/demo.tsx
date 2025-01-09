bb17ccdfa19c279ab97bdc29412a709c19d8f66a
lewis stevens <lewis.stevens1998@hotmail.com>
2025-01-09 12:00:19 +0000

    Add link to docs website

---
+++ packages/website/src/usage/link/basic/demo.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { AdmiraltyLink } from "@ukho/admiralty-react";
+
+export default function Demo() {
+  return <AdmiraltyLink href="www.google.com">Go here</AdmiraltyLink>;
+}
