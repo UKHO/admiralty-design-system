c758c3c78d19c415b09e14cd28f1e75867e784c8
ENGINEERING\MistryN <24655342+developernm@users.noreply.github.com>
2025-06-18 13:23:51 +0100

    feat: side bar for website

--- packages/website/src/app/patterns/layout.tsx
+++ packages/website/src/app/patterns/layout.tsx
@@ -1,19 +1,10 @@
 "use client";
 import React from "react";
 import styles from "./styles.module.css";
-import SideNav, { SideNavItem } from "@/components/side-nav/side-nav";
-
-const sideNavItems: SideNavItem[] = [
-  { path: "/", title: "Overview", },
-  { path: "/service-unavailable", title: "Service Unavailable" },
-  { path: "/page-not-found", title: "Page Not Found" },
-  { path: "service-error", title: "Service Error" },
-];
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className={styles.contentContainer}>
-      <SideNav rootPath="/patterns" items={sideNavItems}/>
       <div className={styles.content}>{children}</div>
     </div>
   );
