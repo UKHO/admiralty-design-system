93b083a2dbcbc863ff305d6598b170cb0ec11328
Lewis Stevens <46570686+lstevens98@users.noreply.github.com>
2025-07-14 09:54:17 +0100

    Fix Icon Button Hoverstate + Add Borderless Icon Variant (#382)
    * Update Icon button css
    
    * Add Storybook Variants
    
    Added secondary and disabled button icon variants to storybook
    
    * Add Icon Secondary button example
    
    * Add secondary icon to docs site
    
    * Add borderless icon button variant
    
    * Add borderless icon button example to docs site
    
    * Add icon-secondary button type
    
    * Delete duplicate button variables
    
    * Update disabled icon background colour

---
+++ packages/website/src/usage/button/borderless-icon/demo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
+import { AdmiraltyButton } from "@ukho/admiralty-react";
+export default function Demo() {
+  return <AdmiraltyButton variant="icon-secondary" icon="refresh" borderless={true}></AdmiraltyButton>;
+}
+
