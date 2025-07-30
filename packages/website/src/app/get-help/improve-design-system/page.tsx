bfea1f93af68045c85f48da3da4a922cb5791667
ENGINEERING\MistryN <24655342+developernm@users.noreply.github.com>
2025-07-30 16:26:22 +0100

    feat: updated links on pages

--- packages/website/src/app/get-help/improve-design-system/page.tsx
+++ packages/website/src/app/get-help/improve-design-system/page.tsx
@@ -3,14 +3,25 @@ import ImproveDesignSystemPage from "./improve-design-system.mdx";
 import ProposeChangeSection from "./propose-change.mdx";
 import ReportBugSection from "./report-bug.mdx";
 import { AdmiraltyButton } from "@ukho/admiralty-react/dist";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
+
+  const proposeChangeNavigate = () => {
+    window.open('https://github.com/UKHO/admiralty-design-system/issues/new?template=feature_request.md', '_blank');
+  };
+
+  const reportBugNavigate = () => {
+    window.open('https://github.com/UKHO/admiralty-design-system/issues/new?template=bug_report.md', '_blank');
+  };
+
   return (
     <div>
       <ImproveDesignSystemPage></ImproveDesignSystemPage>
       <ProposeChangeSection></ProposeChangeSection>
-      <AdmiraltyButton>Propose a change</AdmiraltyButton>
+      <AdmiraltyButton onClick={proposeChangeNavigate}>Propose a change</AdmiraltyButton>
       <ReportBugSection></ReportBugSection>
-      <AdmiraltyButton>Report a bug</AdmiraltyButton>
+      <AdmiraltyButton onClick={reportBugNavigate}>Report a bug</AdmiraltyButton>
     </div>
   );
 }
