1fc2fd3f12a46fc87fe25b5854de2bfac0f0d8f6
Tom Barham <24593464+ecstaticrainbow@users.noreply.github.com>
2025-09-08 10:22:22 +0100

    Removing the webfonts directories from the repo (#394)

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -64,10 +64,7 @@ export const config: Config = {
     {
       type: 'dist',
       esmLoaderPath: '../loader',
-      copy: [
-        { src: '../src/styles/webfonts', dest: '../../styles/webfonts' },
-        { src: '../src/themes', dest: '../../themes' },
-      ],
+      copy: [{ src: '../src/themes', dest: '../../themes' }],
     },
     {
       type: 'dist-custom-elements',
