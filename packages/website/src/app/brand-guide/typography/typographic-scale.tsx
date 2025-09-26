e711556fdd829ac25d05ac21fb44bf14fb357f1d
ENGINEERING\MistryN <24655342+developernm@users.noreply.github.com>
2025-09-26 16:29:55 +0100

    feat: updated typography page v1

--- packages/website/src/app/brand-guide/typography/typographic-scale.tsx
+++ packages/website/src/app/brand-guide/typography/typographic-scale.tsx
@@ -4,58 +4,68 @@ export default function TypographicScale() {
   return (
     <div className={styles.container}>
       <section className={styles.section}>
-        <h3>Heading 1</h3>
-        <h1>The quick brown fox jumped over the lazy dog.</h1>
+        <h3>Heading 1 - Large</h3>
+        <h1 className={styles.h1Large}>The quick brown fox jumped over the lazy dog.</h1>
       </section>
       <section className={styles.section}>
-        <h3>Heading 2</h3>
-        <h2>The quick brown fox jumped over the lazy dog.</h2>
+        <h3>Heading 1 - Medium</h3>
+        <h1 className={styles.h1Medium}>The quick brown fox jumped over the lazy dog.</h1>
       </section>
       <section className={styles.section}>
-        <h3>Heading 3,4,5</h3>
-        <h3>The quick brown fox jumped over the lazy dog.</h3>
+        <h3>Heading 1 - Small</h3>
+        <h1 className={styles.h1Small}>The quick brown fox jumped over the lazy dog.</h1>
+      </section>
+
+      <section className={styles.section}>
+        <h3>Heading 2 - Large</h3>
+        <h2 className={styles.h2Large}>The quick brown fox jumped over the lazy dog.</h2>
       </section>
       <section className={styles.section}>
-        <h3>Heading 6</h3>
-        <h6>The quick brown fox jumped over the lazy dog.</h6>
+        <h3>Heading 2 - Medium</h3>
+        <h2 className={styles.h2Medium}>The quick brown fox jumped over the lazy dog.</h2>
       </section>
       <section className={styles.section}>
-        <h3>Intro Text</h3>
-        <p className="intro">The quick brown fox jumped over the lazy dog.</p>
+        <h3>Heading 2 - Small</h3>
+        <h2 className={styles.h2Small}>The quick brown fox jumped over the lazy dog.</h2>
+      </section>
+
+      <section className={styles.section}>
+        <h3>Heading 3,4,5 - Large</h3>
+        <h3 className={styles.h345Large}>The quick brown fox jumped over the lazy dog.</h3>
       </section>
       <section className={styles.section}>
-        <h3>Paragraph</h3>
-        <p>The quick brown fox jumped over the lazy dog.</p>
+        <h3>Heading 3,4,5 - Medium</h3>
+        <h3 className={styles.h345Medium}>The quick brown fox jumped over the lazy dog.</h3>
       </section>
       <section className={styles.section}>
-        <h3>Small</h3>
-        <p className="small">The quick brown fox jumped over the lazy dog.</p>
+        <h3>Heading 3,4,5 - Small</h3>
+        <h3 className={styles.h345Small}>The quick brown fox jumped over the lazy dog.</h3>
+      </section>
+
+      <section className={styles.section}>
+        <h3>Label - Large</h3>
+        <label className={styles.labelLarge}>The quick brown fox jumped over the lazy dog.</label>
       </section>
       <section className={styles.section}>
-        <h3>Link</h3>
-        <a href="#">The quick brown fox jumped over the lazy dog.</a>
+        <h3>Label - Medium</h3>
+        <label className={styles.labelMedium}>The quick brown fox jumped over the lazy dog.</label>
       </section>
       <section className={styles.section}>
-        <h3>Block Quote</h3>
-        <blockquote>The quick brown fox jumped over the lazy dog.</blockquote>
+        <h3>Label - Small</h3>
+        <label className={styles.labelSmall}>The quick brown fox jumped over the lazy dog.</label>
+      </section>
+
+      <section className={styles.section}>
+        <h3>Paragraph - Small</h3>
+        <p className={styles.pLarge}>The quick brown fox jumped over the lazy dog.</p>
       </section>
       <section className={styles.section}>
-        <h3>Bulleted List</h3>
-        <ul>
-          <li>Lorem ipsum dolor sit amet, consectetur adipiscing.</li>
-          <li>Dolore magna aliqua. Ut enim ad minim veniam.</li>
-          <li>Commodo consequat duis aute irure dolor in.</li>
-          <li>Labore et dolore magna aliqua occaecat.</li>
-        </ul>
+        <h3>Paragraph - Medium</h3>
+        <p className={styles.pMedium}>The quick brown fox jumped over the lazy dog.</p>
       </section>
       <section className={styles.section}>
-        <h3>Numbered List</h3>
-        <ol>
-          <li>Lorem ipsum dolor sit amet, consectetur adipiscing.</li>
-          <li>Dolore magna aliqua. Ut enim ad minim veniam.</li>
-          <li>Commodo consequat duis aute irure dolor in.</li>
-          <li>Labore et dolore magna aliqua occaecat.</li>
-        </ol>
+        <h3>Paragraph - Large</h3>
+        <p className={styles.pSmall}>The quick brown fox jumped over the lazy dog.</p>
       </section>
     </div>
   );
