313cd0b2ae11c99903ac45ffa79ea8e5fa3fb5d2
lewis stevens <lewis.stevens1998@hotmail.com>
2025-10-20 11:35:50 +0100

    Fix formatting

--- packages/website/src/components/colour-blocks/colour-blocks.tsx
+++ packages/website/src/components/colour-blocks/colour-blocks.tsx
@@ -5,33 +5,35 @@ import { useRouter } from "next/navigation";
 export default function ColourBlocks() {
   const router = useRouter();
   return (
-    <div className={styles.colourBlocksContainer}>
+    <div className={styles.homePageContainer}>
       <div className={styles.latestUpdates}>
         <h2>Updates</h2>
         <h3>Last Updated October 2025</h3>
         <p>We released ADMIRALTY Design System update 5.0 on the 8th October 2025. This major update brings enhancements to typography, accessibility, and the website itself. It also introduces serveral new components, including a Pill, a Minified Footer, and new Icon Sidebar and Text Sidebar components.</p>
       </div>
-      <AdmiraltyColourBlock
-        width={434}
-        height={434}
-        colour="teal"
-        heading="Help us improve our design system"
-        actionText="Contact us"
-        enableCardEvent={true}
-        onColourBlockLinkClicked={() => router.push("/get-help/contact-us")}>
-        Share your research or feedback on our github, propose a change or report a bug.
-      </AdmiraltyColourBlock>
-      <AdmiraltyColourBlock
-        width={434}
-        height={434}
-        colour="bright-blue"
-        heading="Figma kit"
-        suppressRedirect={true}
-        enableCardEvent={false}
-        actionText="Coming soon"
-        onColourBlockLinkClicked={() => router.push("#")}>
-        Our Figma kits helps people create designs that are consistent with one another. They are maintained by the ADMIRALTY UKHO Design Team
-      </AdmiraltyColourBlock>
+      <div className={styles.colourBlocksContainer}>
+        <AdmiraltyColourBlock
+          width={434}
+          height={434}
+          colour="teal"
+          heading="Help us improve our design system"
+          actionText="Contact us"
+          enableCardEvent={true}
+          onColourBlockLinkClicked={() => router.push("/get-help/contact-us")}>
+          Share your research or feedback on our github, propose a change or report a bug.
+        </AdmiraltyColourBlock>
+        <AdmiraltyColourBlock
+          width={434}
+          height={434}
+          colour="bright-blue"
+          heading="Figma kit"
+          suppressRedirect={true}
+          enableCardEvent={false}
+          actionText="Coming soon"
+          onColourBlockLinkClicked={() => router.push("#")}>
+          Our Figma kits helps people create designs that are consistent with one another. They are maintained by the ADMIRALTY UKHO Design Team
+        </AdmiraltyColourBlock>
+      </div>
     </div>
   );
 }
