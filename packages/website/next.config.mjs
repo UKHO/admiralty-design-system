38f1bf871d6f410806085ba300df45911b98c8a0
Tom Barham <24593464+ecstaticrainbow@users.noreply.github.com>
2025-12-18 14:12:11 +0000

    Upgrading next js to latest version

--- packages/website/next.config.mjs
+++ packages/website/next.config.mjs
@@ -1,6 +1,3 @@
-import remarkGfm from 'remark-gfm';
-import remarkToc from 'remark-toc';
-import rehypeSlug from 'rehype-slug';
 import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
@@ -11,8 +8,8 @@ const nextConfig = {
 const withMDX = createMDX({
     // Add markdown plugins here, as desired
     options: {
-        remarkPlugins:  [remarkGfm,[remarkToc, {tight: true, heading: 'Contents' }]],
-        rehypePlugins: [rehypeSlug]
+        remarkPlugins:  ["remark-gfm" ,["remark-toc", {tight: true, heading: 'Contents' }]],
+        rehypePlugins: ["rehype-slug"]
     },
 });
 
