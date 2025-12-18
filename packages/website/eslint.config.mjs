2785b16276381f2697f8ed499f741b45463c3d99
Tom Barham <24593464+ecstaticrainbow@users.noreply.github.com>
2025-12-18 14:15:57 +0000

    Adding the typescript es lint config

--- packages/website/eslint.config.mjs
+++ packages/website/eslint.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTypescript from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+  ...nextTypescript,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
