3effa4e7b66b97293d3b2f5f9a90a3986bc8b723
Tom Barham <24593464+ecstaticrainbow@users.noreply.github.com>
2026-01-09 14:40:15 +0000

    Fixing warning about not being able to find the correct base path

--- packages/website/next.config.mjs
+++ packages/website/next.config.mjs
@@ -1,8 +1,16 @@
 import createMDX from '@next/mdx';
+import path from "node:path";
+import {fileURLToPath} from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
 };
 
 const withMDX = createMDX({
