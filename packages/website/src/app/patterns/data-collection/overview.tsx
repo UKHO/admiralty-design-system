2a9883c663e6c718c591f19a79d37ab277955694
Lewis Stevens <46570686+lstevens98@users.noreply.github.com>
2026-01-26 09:56:57 +0000

    Find A Location and Data Collection pages (#435)
    * Add page routing and text
    
    * Add Images
    
    * Update intro paragraph styling
    
    * Update map controls image
    
    * Add borders to images
    
    * Update border styling to be consistent with playground snippets
    
    * Add line height for intro text

--- packages/website/src/app/patterns/data-collection/overview.tsx
+++ packages/website/src/app/patterns/data-collection/overview.tsx
@@ -1,10 +1,15 @@
+import styles from "./styles.module.css"
 import Image from "next/image";
 
 export default function DataCollectionImages() {
   return (
     <div style={{ display: "flex", gap: '10rem'}}>
-      <Image src="/svg/Survey-details.svg" width={500} height={900} alt="UKHO Logo" />
-      <Image src="/svg/Intellectual-property.svg" width={500} height={900} alt="UKHO Logo" />
+      <div className={styles.imageWrapper}>
+        <Image src="/svg/Survey-details.svg" width={500} height={900} alt="UKHO Logo" />
+      </div>
+      <div className={styles.imageWrapper}>
+        <Image src="/svg/Intellectual-property.svg" width={500} height={900} alt="UKHO Logo" />
+      </div>
     </div>
   );
 }
