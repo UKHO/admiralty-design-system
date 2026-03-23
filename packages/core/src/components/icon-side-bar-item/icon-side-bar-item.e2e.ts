cc300b8b0e31faeb8f5657f60735500fb35c5546
lewis stevens <lewis.stevens1998@hotmail.com>
2026-03-23 15:57:51 +0000

    fix tests

--- packages/core/src/components/icon-side-bar-item/icon-side-bar-item.e2e.ts
+++ packages/core/src/components/icon-side-bar-item/icon-side-bar-item.e2e.ts
@@ -4,8 +4,8 @@ describe('icon-side-nav-item', () => {
   it('renders', async () => {
     const page = await newE2EPage();
 
-    await page.setContent('<admiralty-icon-side-nav-item></admiralty-icon-side-nav-item>');
-    const element = await page.find('admiralty-icon-side-nav-item');
+    await page.setContent('<admiralty-icon-side-bar-item></admiralty-icon-side-bar-item>');
+    const element = await page.find('admiralty-icon-side-bar-item');
     expect(element).toHaveClass('hydrated');
   });
 });
